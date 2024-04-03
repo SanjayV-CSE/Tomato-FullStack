4191739e377e105eca77fe51a88d42cd68431830
sanjay_v2021cseb <sanjay.v2021cseb@sece.ac.in>
2024-04-03 20:18:32 +0530

    simple changes

--- food-del/src/components/Header/Header.jsx
+++ food-del/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='py-6'>
-      <div className="h-[600px] bg-contain bg-no-repeat bg-[url('C:\Users\sanju\OneDrive\Desktop\FOOD\food-del\public\header_img.png')] p-5 text-white space-y-5 flex flex-col justify-center">
+      <div className="h-[600px] bg-contain bg-no-repeat bg-[url('C:\Users\sanju\Tomato-FullStack\food-del\public\header_img.png')] p-5 text-white space-y-5 flex flex-col justify-center">
         <h1 className='text-6xl font-bold'>Order Your Favourite Food Here</h1>
         <p>Explore our diverse menu, order effortlessly, and enjoy delectable dishes <br />
         delivered promptly to your doorstep for an <br />unparalleled dining experience.</p>
