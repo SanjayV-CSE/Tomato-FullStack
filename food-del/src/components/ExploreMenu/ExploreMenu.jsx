15c8b067a650b0a8c609cf18dd534b7bc17b5592
sanjay_v2021cseb <sanjay.v2021cseb@sece.ac.in>
2024-04-03 13:52:35 +0530

    store

--- food-del/src/components/ExploreMenu/ExploreMenu.jsx
+++ food-del/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { menu_list } from '../../assets/assets'
-const ExploreMenu = () => {
+const ExploreMenu = ({category, setCategory}) => {
   return (
     <div className='space-y-8'>
       <h1 className='text-4xl font-bold'>Explore Our Menu</h1>
       <p>Dive into a world of culinary delights! Explore our diverse menu, overflowing with delicious options to satisfy every craving. From <br />classic favorites to innovative creations, we have something to tantalize every taste bud. Don't wait, embark on your <br />delicious journey today! </p>
       <div className='flex justify-center gap-8'>
-        {menu_list.map((item,index)=>{
+        {menu_list.map((item,index)=>{ 
           return (
-            <div key={index}>
-              <img className='cursor-pointer' src={item.menu_image} alt="" />
+            <div  onClick={() => setCategory(prev => prev === item.menu_name ? 'All' : item.menu_name)} key={index}>
+              <img className={`cursor-pointer ${category === item.menu_name ? 'animate-pulse' : ''}`} src={item.menu_image} alt="" />
               <p className='flex justify-center'>{item.menu_name}</p>
             </div>
           )
